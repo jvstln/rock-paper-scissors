21693be38444f07b827062e7349d4b72aefef5c8
jvstln <173433315+jvstln@users.noreply.github.com>
2024-10-01 19:43:10 +0100

    Add playRound function(ality)

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let humanScore = 0;
+
 function getComputerChoice() {
   const rps = ["rock", "paper", "scissors"];
   const randomIndex = Math.floor(Math.random() * 3);
@@ -7,4 +10,26 @@ function getComputerChoice() {
 function getHumanChoice() {
   const choice = prompt("Choose one (rock, paper, scissors)?");
   return choice.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("Draw! You both picked " + humanChoice);
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log(`You WIN! ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+  } else if (
+    (computerChoice === "rock" && humanChoice === "scissors") ||
+    (computerChoice === "paper" && humanChoice === "rock") ||
+    (computerChoice === "scissors" && humanChoice === "paper")
+  ) {
+    console.log(`You LOSE! ${computerChoice} beats ${humanChoice}`);
+    computerScore++;
+  } else {
+    console.log(humanChoice + " is an INVALID choice!");
+  }
 }
